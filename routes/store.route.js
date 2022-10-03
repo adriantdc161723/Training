f3d24a91d106626710db4eec118997982148f92f
AdrianTDC <adriantdc2022@gmail.com>
2022-10-03 11:28:33 +0800

    another commit

--- routes/store.route.js
+++ routes/store.route.js
@@ -25,23 +25,92 @@ StoreRouter.get('/get-all-stores', async (req, res)=>{
 });
 
 StoreRouter.get('/get-one-store/:id', async (req, res)=>{
-    const response = await StoreController.getOneStore(parseInt(req.params.id));
-    return res.status(response.status).send(response);
+    // const response = await StoreController.getOneStore(parseInt(req.params.id));
+    // return res.status(response.status).send(response);
+    if(req.headers.authorization){
+        const token = req.headers.authorization.split(" ")[1];
+        let authenticate = await AuthService.verify(token);
+
+        if(authenticate.status === 200){
+
+            const response = await StoreController.getOneStore(parseInt(req.params.id));
+            return res.status(response.status).send(response);
+
+        }else{
+            return res.status(authenticate.status).send(authenticate);
+        }
+
+    }else{
+        return res.status(400).send({ message: "No Token!" });
+    }
 });
 
 StoreRouter.post('/create-store', async (req, res)=>{
-    const response = await StoreController.createStore(req.body);
-    return res.status(response.status).send(response);
+    // const response = await StoreController.createStore(req.body);
+    // return res.status(response.status).send(response);
+    if(req.headers.authorization){
+        const token = req.headers.authorization.split(" ")[1];
+        let authenticate = await AuthService.verify(token);
+
+        if(authenticate.status === 200){
+
+            const response = await StoreController.createStore(req.body);
+            return res.status(response.status).send(response);
+
+        }else{
+            return res.status(authenticate.status).send(authenticate);
+        }
+
+    }else{
+        return res.status(400).send({ message: "No Token!" });
+    }
+
 });
 
 StoreRouter.put('/update-store', async (req, res)=>{
-    const response = await StoreController.updateStore(req.body);
-    return res.status(response.status).send(response);
+    // const response = await StoreController.updateStore(req.body);
+    // return res.status(response.status).send(response);
+
+    if(req.headers.authorization){
+        const token = req.headers.authorization.split(" ")[1];
+        let authenticate = await AuthService.verify(token);
+
+        if(authenticate.status === 200){
+
+            const response = await StoreController.updateStore(req.body);
+            return res.status(response.status).send(response);
+
+        }else{
+            return res.status(authenticate.status).send(authenticate);
+        }
+
+    }else{
+        return res.status(400).send({ message: "No Token!" });
+    }
+
 });
 
 StoreRouter.delete('/delete-store/:id', async (req, res)=>{
-    const response = await StoreController.deleteStore(parseInt(req.params.id));
-    return res.status(response.status).send(response);
+    // const response = await StoreController.deleteStore(parseInt(req.params.id));
+    // return res.status(response.status).send(response);
+
+    if(req.headers.authorization){
+        const token = req.headers.authorization.split(" ")[1];
+        let authenticate = await AuthService.verify(token);
+
+        if(authenticate.status === 200){
+
+            const response = await StoreController.deleteStore(parseInt(req.params.id));
+            return res.status(response.status).send(response);
+
+        }else{
+            return res.status(authenticate.status).send(authenticate);
+        }
+
+    }else{
+        return res.status(400).send({ message: "No Token!" });
+    }
+
 });
 
 
